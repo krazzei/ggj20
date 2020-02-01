bfa3fa96a77f270dbc6e8ecb5c3ca397da8c125b
Tyler White <twigwhite@gmail.com>
2020-02-01 10:39:10 -0600

    Initial commit! Have a basic turn based battle system. Starting player logic

---
+++ Assets/Code/PlayerController.cs
@@ -0,0 +1,13 @@
+public class PlayerController : Controller
+{
+	protected override void StartTurn()
+	{
+		// TODO: enable menu?
+		// We just wait until the menu tells us what the user wants to do.
+	}
+	
+	public void HealTarget(Controller target)
+	{
+		
+	}
+}
---
+++ Assets/Code/TurnBasedBattleSystem.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnBasedBattleSystem : MonoBehaviour
+{
+    public Controller playerControllerPrefab;
+    public Controller enemyControllerPrefab;
+    
+    private readonly List<Controller> _controllers = new List<Controller>();
+    private int _controllerIndex;
+    
+    private void Start()
+    {
+        var player = Instantiate(playerControllerPrefab);
+        var enemy = Instantiate(enemyControllerPrefab);
+        
+        _controllers.Add(player);
+        _controllers.Add(enemy);
+
+        _controllerIndex = 0;
+        _controllers[_controllerIndex].TakeTurn(NextTurn);
+    }
+
+    private void NextTurn()
+    {
+        if (++_controllerIndex >= _controllers.Count)
+        {
+            _controllerIndex = 0;
+        }
+        
+        _controllers[_controllerIndex].TakeTurn(NextTurn);
+    }
+}
